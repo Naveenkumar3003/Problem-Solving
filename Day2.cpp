290a9fddeef3c8a4a90935e33b11538152b0d23f
Naveenkumar N <naveengoogl2006@gmail.com>
2026-01-02 07:05:18 +0530

    Create Day2.cpp

---
+++ Day2.cpp
@@ -0,0 +1,12 @@
+961. N-Repeated Element in Size 2N Array
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int x:nums){
+            if(!s.insert(x).second) return x;
+        }
+        return -1;
+    }
+};
