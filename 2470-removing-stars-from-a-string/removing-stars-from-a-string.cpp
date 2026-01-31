900bb420ca262d62d91f5476d9a513d577845574
Naveenkumar N <naveengoogl2006@gmail.com>
2026-01-31 08:15:16 +0530

    Time: 23 ms (33.92%) | Memory: 28.5 MB (61.97%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string removeStars(string s) {
-        string result;
+        string result="";
         for(char c : s){
             if(c=='*') result.pop_back();
             else result.push_back(c);
