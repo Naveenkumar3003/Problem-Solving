d114294381d506d82086ac1ca78f213da291e1c5
Naveenkumar N <naveengoogl2006@gmail.com>
2026-07-09 18:23:27 +0530

    Add Quick Sort implementation in Quick_sort.cpp
    Implement Quick Sort algorithm to sort an array.

---
+++ Quick_sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int partition(vector<int> &arr,int low, int high){
+    int i=low,j=high;
+    int pivot=arr[low];
+    while(i<j){
+        while(arr[i]<=pivot && i<high){
+            i++;
+        }
+        while(arr[j]>pivot && j>low){
+            j--;
+        }
+        if(i<j) swap(arr[i],arr[j]);
+    }
+    swap(arr[j],arr[low]);
+    return j;
+}
+void qs(vector<int> &arr,int low,int high){
+    int p;
+    if(low<high){
+        p=partition(arr,low,high);
+        qs(arr,low,p-1);
+        qs(arr,p+1,high);
+    }
+}
+int main() {
+    vector<int> arr={4,2,7,1,8,3};
+    qs(arr,0,arr.size()-1);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
