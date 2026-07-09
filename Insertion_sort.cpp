78c0755f3e7eb4440ca2ec516170cca0b8f779ad
Naveenkumar N <naveengoogl2006@gmail.com>
2026-07-09 18:35:33 +0530

    Create Insertion_sort.cpp

---
+++ Insertion_sort.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void i_sort(vector<int> &arr){
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
+}
+int main() {
+    vector<int> arr={4,2,7,1,8,3};
+    i_sort(arr);
+    for(int i=0;i<arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
