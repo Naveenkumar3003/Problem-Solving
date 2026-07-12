86430e2ca77c237b4b5e9f1b2ac4b2b748ad01db
Naveenkumar N <naveengoogl2006@gmail.com>
2026-07-12 18:03:09 +0530

    Add topView function to Solution class
    Implement top view of a binary tree using BFS.

---
+++ tree_top_view.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        vector<int> ans;
+        if(root==nullptr) return ans;
+        unordered_map<int,int> mp;
+        queue<pair<Node*,int>> q;
+        int minhd=0,maxhd=0;
+        q.push({root,0});
+        while(!q.empty()){
+            auto front=q.front();
+            q.pop();
+            Node* node=front.first;
+            int hd=front.second;
+            if(mp.find(hd)==mp.end()){
+                mp[hd]=node->data;
+            }
+            minhd=min(minhd,hd);
+            maxhd=max(maxhd,hd);
+            if(node->left) q.push({node->left,hd-1});
+            if(node->right) q.push({node->right,hd+1});
+        }
+        for(int i=minhd;i<=maxhd;i++){
+            ans.push_back(mp[i]);
+        }
+        return ans;
+    }
+};
