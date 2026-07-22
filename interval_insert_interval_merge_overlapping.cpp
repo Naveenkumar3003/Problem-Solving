580b578716db6e72f318fed6fd9fb0729d46d2ae
Naveenkumar N <naveengoogl2006@gmail.com>
2026-07-22 18:51:49 +0530

    Implement interval insertion and merging function

---
+++ interval_insert_interval_merge_overlapping.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> insert_interval_merge_overlapping(vector<vector<int>>& intervals, vector<int>& newInterval) {
+       intervals.push_back(newInterval);
+       sort(intervals.begin(),intervals.end());
+       vector<vector<int>> res;
+       res.push_back(intervals[0]);
+       for(int i=1;i<intervals.size();i++){
+           vector<int>& last=res.back();
+           vector<int> curr=intervals[i];
+           if(curr[0]<=last[1]) last[1]=max(last[1],curr[1]);
+           else res.push_back(curr);
+       }
+       return res;
+    }
+};
