ccd8204b0b5396332f40a636214697b5d9bd264d
Naveenkumar N <naveengoogl2006@gmail.com>
2026-07-30 22:21:25 +0530

    Add reverseStack and insertAtBottom methods
    Implemented reverseStack function to reverse a stack using recursion.

---
+++ Recursion/reverseStack.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    void insertAtBottom(stack<int> &st,int x){
+        if(st.empty()){
+            st.push(x);
+            return;
+        }
+        int val=st.top();
+        st.pop();
+        insertAtBottom(st,x);
+        st.push(val);
+    }
+    void reverseStack(stack<int> &st) {
+        // code here
+        if(st.empty()) return;
+        int x=st.top();
+        st.pop();
+        reverseStack(st);
+        insertAtBottom(st,x);
+    }
+};
