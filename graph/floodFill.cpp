7fb306c5cf6be6c1133b72c339af20fdd82217f3
Naveenkumar N <naveengoogl2006@gmail.com>
2026-07-31 18:25:59 +0530

    Create floodFill.cpp

---
+++ graph/floodFill.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size();
+        int m=image[0].size();
+        int org=image[sr][sc];
+        if(org==color) return image;
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        image[sr][sc]=color;
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+        while(!q.empty()){
+            auto [r,c]=q.front();
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==org){
+                    image[nr][nc]=color;
+                    q.push({nr,nc});
+                }
+            }
+        }
+        return image;
+    }
+};
