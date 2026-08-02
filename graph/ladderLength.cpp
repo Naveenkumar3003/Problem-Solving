7a52cf6380baeae053b78c3fdaff9d8b215e4503
Naveenkumar N <naveengoogl2006@gmail.com>
2026-08-02 10:45:59 +0530

    Implement ladderLength function for word transformation

---
+++ graph/ladderLength.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> q;
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        q.push({beginWord,1});
+        while(!q.empty()){
+            string top=q.front().first;
+            int level=q.front().second;
+            q.pop();
+            if(top==endWord) return level;
+            for(int i=0;i<top.length();i++){
+                for(char c='a';c<='z';c++){
+                    string word=top;
+                    word[i]=c;
+                    if(st.find(word)!=st.end()){
+                        q.push({word,level+1});
+                        st.erase(word);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
