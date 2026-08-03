4b01bee4d12d71db26b4a67ec001ebaf1a20828e
Naveenkumar N <naveengoogl2006@gmail.com>
2026-08-03 06:44:45 +0530

    Update checkSubsequenceSum.cpp

--- Recursion/checkSubsequenceSum.cpp
+++ Recursion/checkSubsequenceSum.cpp
@@ -1,18 +1,19 @@
 class Solution {
-  public:
-    bool func(int ind, int sum, std::vector<int> &nums) {
-        // Base case: if all elements are processed, check if sum is 0
-        if (ind == nums.size()) {
-            return sum == 0;
-        }
-        // Recursive call: include the current element in the subsequence
-        // or exclude the current element from the subsequence
-        return func(ind + 1, sum - nums[ind], nums) | func(ind + 1, sum, nums);
+private:
+    bool func(int ind, int sum, vector<int> &nums) {
+        // Target achieved
+        if (sum == 0) return true;
+
+        // Out of bounds or sum became negative
+        if (sum < 0 || ind == nums.size()) return false;
+
+        // Include current element OR exclude it
+        return func(ind + 1, sum - nums[ind], nums) ||
+               func(ind + 1, sum, nums);
     }
 
-    public:
-        // This method initiates the recursive process
-        bool checkSubsequenceSum(std::vector<int>& nums, int target) {
-            return func(0, target, nums); // Start the recursive process
-        }
+public:
+    bool isSubsequenceWithTargetSumPresent(vector<int>& nums, int target) {
+        return func(0, target, nums);
+    }
 };
