0f7880c41a33d19ae5093adb0796870b48f5cabd
Naveenkumar N <naveengoogl2006@gmail.com>
2026-08-04 21:33:43 +0530

    Update dijkstra.cpp

--- graph/shortest_path/dijkstra.cpp
+++ graph/shortest_path/dijkstra.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
-        // Code here
+        
         vector<vector<pair<int,int>>> adj(V);
         for(auto edge: edges){
             int u=edge[0];
@@ -10,22 +10,45 @@ class Solution {
             adj[u].push_back({v,w});
             adj[v].push_back({u,w});
         }
+        // MIN HEAP
+        // priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        // vector<int> dist(V,1e9);
         
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        // dist[src]=0;
+        // pq.push({0,src});
+        // while(!pq.empty()){
+        //     int dis=pq.top().first;
+        //     int node=pq.top().second;
+        //     pq.pop();
+        //     for(auto neigh:adj[node]){
+        //         int edgeW=neigh.second;
+        //         int adjNode=neigh.first;
+        //         if(edgeW+dis<dist[adjNode]){ 
+        //             dist[adjNode]=edgeW+dis;
+        //             pq.push({dist[adjNode],adjNode});
+        //         }
+        //     }
+        // }
+        // return dist;
+        
+        // SET
+        set<pair<int,int>> st;
         vector<int> dist(V,1e9);
-        
         dist[src]=0;
-        pq.push({0,src});
-        while(!pq.empty()){
-            int dis=pq.top().first;
-            int node=pq.top().second;
-            pq.pop();
-            for(auto neigh:adj[node]){
+        st.insert({0,src});
+        while(!st.empty()){
+            auto elem=*(st.begin());
+            int dis=elem.first;
+            int node=elem.second;
+            st.erase(elem);
+            
+            for(auto neigh: adj[node]){
                 int edgeW=neigh.second;
                 int adjNode=neigh.first;
-                if(edgeW+dis<dist[adjNode]){ 
+                if(dis+edgeW<dist[adjNode]){
+                    if(dist[adjNode]!=1e9) st.erase({dist[adjNode],adjNode});
                     dist[adjNode]=edgeW+dis;
-                    pq.push({dist[adjNode],adjNode});
+                    st.insert({dist[adjNode],adjNode});
                 }
             }
         }
