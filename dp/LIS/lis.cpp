2dea1b290c150e31485d0a2f809e65bc5994507a
Naveenkumar N <naveengoogl2006@gmail.com>
2026-08-30 14:07:21 +0530

    Implement LIS using dynamic programming

---
+++ dp/LIS/lis.cpp
@@ -0,0 +1,48 @@
+class Solution {
+  public:
+    /*int liseq(int ind,int prev_ind,vector<int> &arr){
+        if(ind==arr.size()) return 0;
+        int nottake=liseq(ind+1,prev_ind,arr);
+        int take=0;
+        if(prev_ind==-1 || arr[ind]>arr[prev_ind]) {
+            take=1+liseq(ind+1,ind,arr);
+        }
+        return max(nottake,take);
+    }
+    int lis(vector<int>& arr) {
+        return liseq(0,-1,arr);
+    }*/
+    
+    
+    /*int liseq(int ind,int prev_ind,vector<int> &arr,
+                vector<vector<int>> &dp){
+        if(ind==arr.size()) return 0;
+        if(dp[ind][prev_ind+1]!=-1) return dp[ind][prev_ind+1];
+        int nottake=liseq(ind+1,prev_ind,arr,dp);
+        int take=0;
+        if(prev_ind==-1 || arr[ind]>arr[prev_ind]) {
+            take=1+liseq(ind+1,ind,arr,dp);
+        }
+        return dp[ind][prev_ind+1]=max(nottake,take);
+    }
+    int lis(vector<int>& arr) {
+        vector<vector<int>> dp(arr.size(),vector<int>(arr.size()+1,-1));
+        return liseq(0,-1,arr,dp);
+    }*/
+    
+    int lis(vector<int>& arr) {
+        int n=arr.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev_ind=n-1;prev_ind>=-1;prev_ind--){
+                int nottake=dp[ind+1][prev_ind+1];
+                int take=0;
+                if(prev_ind==-1 || arr[ind]>arr[prev_ind]) {
+                    take=1+dp[ind+1][ind+1];
+                }
+                dp[ind][prev_ind+1]=max(nottake,take);
+            }
+        }
+        return dp[0][0];
+    }
+};
